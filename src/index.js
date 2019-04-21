70796225ec501bde3bd6451a3cdf9e933b3391ce
Simon Boeuf <simon.boeuf@gmail.com>
2019-04-21 19:07:54 +0200

    remove react-redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
 import App from './components/App'
-import rootReducer from './reducers'
-import loggerMiddleware from 'redux-logger'
-
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+      <App />
+,  document.getElementById('root')
 )
