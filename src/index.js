3b2a80eb9cfeba72eed6006ec1402d1f857255f5
Simon Boeuf <simon.boeuf@gmail.com>
2019-04-21 14:37:02 +0200

    add sample middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const loggerMiddleware = ({ getState }) => next => (action) => {
+  console.log("current state", getState());
+  console.log("dispatching action", action);
+  const nextState = next(action);
+  console.log("next state", nextState);
+  return nextState;
+}
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 render(
   <Provider store={store}>
