f1f4da1bfc83dc1a43446e62f7b236aa75727086
Simon Boeuf <simon.boeuf@gmail.com>
2019-04-21 14:39:28 +0200

    move middlware in own file

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
-
-const loggerMiddleware = ({ getState }) => next => (action) => {
-  console.log("current state", getState());
-  console.log("dispatching action", action);
-  const nextState = next(action);
-  console.log("next state", nextState);
-  return nextState;
-}
+import loggerMiddleware from './loggerMiddleware'
 
 const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
